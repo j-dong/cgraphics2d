e7c5385ceb39d3a78435db4aad00c80c4522d151
James Dong <dong.james.sept14@gmail.com>
2017-08-13 20:25:09 -0500

    Begin hashtable implementation

---
+++ include/hashtable.h
@@ -0,0 +1,39 @@
+#ifndef INCLUDED_GRAPHICS_HASHTABLE_H
+#define INCLUDED_GRAPHICS_HASHTABLE_H
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// robin-hood hash table for strings
+// see https://www.sebastiansylvan.com/post/robin-hood-hashing-should-be-your-default-hash-table-implementation/
+// and http://codecapsule.com/2013/11/17/robin-hood-hashing-backward-shift-deletion/
+
+#define HASHTABLE_KEY char *
+
+// if key is NULL then empty
+struct hashtable_t {
+    // capacity is always a power of 2
+    // initial capacity is specified in hashtable.c
+    size_t data_len,
+           data_cap,
+           resize_cap;
+    // mask = capacity - 1
+    // so we can do a bitwise AND
+    uint64_t mask;
+    struct hashtable_entry_t {
+        HASHTABLE_KEY key;
+        void *value;
+        uint64_t hash;
+    } *data;
+};
+
+typedef struct hashtable_t Hashtable;
+
+void hashtable_init(Hashtable *h);
+void hashtable_delete(Hashtable *h);
+void hashtable_insert(Hashtable *h, HASHTABLE_KEY key, void *value);
+void *hashtable_get(Hashtable *h, HASHTABLE_KEY key);
+void hashtable_remove(Hashtable *h, HASHTABLE_KEY key);
+
+#endif
---
+++ src/hashtable.c
@@ -0,0 +1,81 @@
+#include "hashtable.h"
+
+// hash table implementation
+
+// hash function
+
+// hash function makes assumptions about target endianness
+#if defined(_MSC_VER)
+#if !defined(_M_X64)
+#error "MurmurHash3 is optimized for x86-64 systems."
+#endif
+#elif defined(__GNUC__)
+#if !defined(__x86_64__)
+#error "MurmurHash3 is optimized for x86-64 systems."
+#endif
+#else
+#error "Cannot detect target architecture. " \
+       "If you are compiling for x86-64, " \
+       "please remove and ignore this error."
+#endif
+
+static inline uint64_t rotl64(uint64_t x, uint64_t r) {
+    return (x << r) | (x >> (64 - r));
+}
+
+static inline uint64_t fmix64(uint64_t k) {
+    k ^= k >> 33;
+    k *= 0xff51afd7ed558ccdull;
+    k ^= k >> 33;
+    k *= 0xc4ceb9fe1a85ec53ull;
+    k ^= k >> 33;
+    return k;
+}
+
+static uint64_t str_hash(const uint8_t *key, const size_t len, const uint32_t seed) {
+    // MurmurHash3
+    // https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp
+    const size_t nblocks = len / 16;
+    uint64_t h1 = seed, h2 = seed;
+    const uint64_t c1 = 0x87c37b91114253d5ull,
+                   c2 = 0x4cf5ad432745937full;
+    const uint64_t *blocks = (const uint64_t *)data;
+    for (int i = 0; i < nblocks; i++) {
+        uint64_t k1 = blocks[i * 2    ],
+                 k2 = blocks[i * 2 + 1];
+        k1 *= c1; k1 = rotl64(k1, 31); k1 *= c2; h1 ^= k1;
+        h1 = rotl64(h1, 27); h1 += h2; h1 = h1 * 5 + 0x52dce729;
+        k2 *= c2; k2 = rotl64(k2, 33); k2 *= c1; h2 ^= k2;
+        h2 = rotl64(h2, 31); h2 += h1; h2 = h2 * 5 + 0x38495ab5;
+    }
+    const uint8_t *tail = (const uint8_t *)(data + nblocks * 16);
+    uint64_t k1 = 0, k2 = 0;
+    switch (len & 15) {
+    case 15: k2 ^= ((uint64_t)tail[14]) << 48;
+    case 14: k2 ^= ((uint64_t)tail[13]) << 40;
+    case 13: k2 ^= ((uint64_t)tail[12]) << 32;
+    case 12: k2 ^= ((uint64_t)tail[11]) << 24;
+    case 11: k2 ^= ((uint64_t)tail[10]) << 16;
+    case 10: k2 ^= ((uint64_t)tail[ 9]) << 8;
+    case  9: k2 ^= ((uint64_t)tail[ 8]) << 0;
+             k2 *= c2; k2 = rotl64(k2, 33); k2 *= c1; h2 ^= k2;
+    case  8: k1 ^= ((uint64_t)tail[ 7]) << 56;
+    case  7: k1 ^= ((uint64_t)tail[ 6]) << 48;
+    case  6: k1 ^= ((uint64_t)tail[ 5]) << 40;
+    case  5: k1 ^= ((uint64_t)tail[ 4]) << 32;
+    case  4: k1 ^= ((uint64_t)tail[ 3]) << 24;
+    case  3: k1 ^= ((uint64_t)tail[ 2]) << 16;
+    case  2: k1 ^= ((uint64_t)tail[ 1]) << 8;
+    case  1: k1 ^= ((uint64_t)tail[ 0]) << 0;
+             k1 *= c1; k1 = rotl64(k1, 31); k1 *= c2; h1 ^= k1;
+    }
+    h1 ^= len;
+    h2 ^= len;
+    h1 += h2;
+    h2 += h1;
+    h1 = fmix64(h1);
+    h2 = fmix64(h2);
+    h1 += h2;
+    h2 += h1;
+    return h1;
+}
